93fa34bc2f7f0274bf5242824bbab3c1553dcc2e
yangx38 <yangx38@uw.edu>
2019-07-19 00:30:57 -0700

    blah

---
+++ location-service/src/main/java/xcode/springcloud/locationservice/Application.java
@@ -0,0 +1,12 @@
+package xcode.springcloud.locationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
